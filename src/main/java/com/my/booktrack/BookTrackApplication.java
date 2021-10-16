387feb783e04f3c3d69ccfb5d3c97570cebb72cb
ld3l <measpnir@gmail.com>
2021-10-16 23:24:43 +0300

    Initial commit

---
+++ src/main/java/com/my/booktrack/BookTrackApplication.java
@@ -0,0 +1,13 @@
+package com.my.booktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/booktrack/BookTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.booktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
